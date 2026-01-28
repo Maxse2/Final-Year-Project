b4bf48f23206cb446575673856399908d9c2a1b0
Max Stott <maxstott4@gmail.com>
2026-01-28 18:45:57 +0000

    Ongoing work on normalising linux auth logs

--- Normalisation/auth_log_normaliser.py
+++ Normalisation/auth_log_normaliser.py
@@ -2,6 +2,8 @@ import re
 from datetime import datetime
 from base_normaliser import BaseNormaliser
 
+# Regular Expression algorithm for parsing auth.log files
+
 auth_log_regex = re.compile(
     r'^(?P<timestamp>\S+)\s+'
     r'(?P<hostname>\S+)\s+'
@@ -9,16 +11,41 @@ auth_log_regex = re.compile(
     r'(?P<message>.*)$'
 )
 
-test_line = (
-    "2026-01-27T16:59:30.983438+00:00 Ubuntu "
-    "systemd-logind[1040]: New seat seat0."
-)
+# Labelling and classification for parsed events
 
-match = auth_log_regex.match(test_line)
+def event_classification(message,service):
+    msg = message.lower()
+    
+    # Classifies password-related events
+    if service == "sshd":
+        if "failed password" in msg:
+            return "FAILED_LOGIN"
+        if "accepted password" in msg:
+            return "SUCCESSFUL_LOGIN"
+    return "OTHER"
 
-if match:
-    print("✅ Match found!\n")
-    for name, value in match.groupdict().items():
-        print(f"{name}: {value}")
-else:
-    print("❌ No match")
+# Main normalisation function
+def normalise_lines(lines):
+    normalised = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        matched = auth_log_regex.match(line)
+        if not matched:
+            continue
+    data = matched.groupdict()
+    event_type = event_classification(data["message"], data["service"])
+    
+    normalised.append({
+        "event_id": f"{data['service'].upper()}_{event_type.upper()}",
+        "event_timestamp": data['timestamp'],
+        "hostname": data['hostname'],
+        "ip_address": None, # Will be revisited later in development
+        "event_type:": event_type,
+        "message": data["message"],
+        "Source": "linux_auth",
+        "service": data['service'],
+        "pid" : data["pid"]
+        })
+    return normalised
